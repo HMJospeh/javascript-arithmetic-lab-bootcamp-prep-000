79411544aecb2c1312fed16a9f02aaac542832fc
Humza Joseph <humza1born@gmail.com>
2017-12-16 23:20:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,107 @@
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+var a= 700, b= 48;
+function add(a, b){
+return a + b;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+  function subtract(a, b){
+    return a - b;
+  }
+  function beforeEach(){
+    a = Math.floor(Math.random() * 1000)
+    b = Math.floor(Math.random() * 1000)
+}
+function multiply( a, b){
+  return a * b;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function divide( a, b){
+  return a / b;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function inc(a){
+  return a + 1;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function dec(a){
+  return a - 1;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function makeInt(a){
+  return a;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+var a= 700, b= 48;
+function add(a, b){
+return a + b;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+  function subtract(a, b){
+    return a - b;
+  }
+  function beforeEach(){
+    a = Math.floor(Math.random() * 1000)
+    b = Math.floor(Math.random() * 1000)
+}
+function multiply( a, b){
+  return a * b;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function divide( a, b){
+  return a / b;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function inc(a){
+  return a + 1;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function dec(a){
+  return a - 1;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function makeInt(a){
+  return a;
+}
+function beforeEach(){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function makeInt(0*2328) {
+  return 0;
+}
