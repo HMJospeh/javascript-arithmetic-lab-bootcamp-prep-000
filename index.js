97a83182c01921e698f60cdba69f6b63d1cb1572
Humza Joseph <humza1born@gmail.com>
2017-12-17 01:18:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var a= 700, b= 48;
 function add(a, b){
 return a + b;
 }
 function subtract(a, b){
     return a - b;
   }
-
 function multiply( a, b){
   return a * b;
 }
@@ -19,10 +17,6 @@ function inc(a){
 function dec(a){
   return a - 1;
 }
-function inc(a){
-  return a + 1;
-}
 function makeInt(a) {
-  return parseInt('.233', 10);
-  return present('nonsense!', 10);
+  return parseInt(a, 10);
 }
